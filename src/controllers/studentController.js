c6edb027f87a160800867e554b5ec8a0792defcd
Kiancis Dominguez <kiancisd@gmail.com>
2022-09-04 17:00:11 -0400

    first commit

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -3,7 +3,7 @@ import Model from "../models/studentModel";
 export function getAll() {
 	return new Promise(async (resolve, reject) => {
 		const students = await Model.getAll();
-		if (students) {
+		if (students.length > 0) {
 			resolve(students);
 		} else {
 			reject("Not students found");
